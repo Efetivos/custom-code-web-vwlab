753ed1e3fe9ac01113d2c125dfbca40bc2481186
Efetivos <30553650+Efetivos@users.noreply.github.com>
2025-01-25 08:45:00 -0500

    Updated

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-gsap.fromTo('.box', { scale: 0 }, { duration: 2, scale: 1, backgroundColor: '#000', yoyo: true, stagger: { each: .1, repeat: -1, yoyo: true, from: 'center' }})
+gsap.fromTo('.box', { scale: 0, backgroundColor: 'white' }, { duration: 2, scale: 1, yoyo: true, stagger: { each: .1, repeat: -1, yoyo: true, from: 'center' }})
