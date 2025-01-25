ce0c9880b5fe2659f2aafa34ee4d2b11e22d5657
Efetivos <30553650+Efetivos@users.noreply.github.com>
2025-01-25 13:33:57 +0000

    Updated Box

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-gsap.fromTo('.box', { yPercent: -100 }, { duration: 2, yPercent: 100, backgroundColor: '#ffe2c5', repeat: -1, yoyo: true })
+gsap.fromTo('.box', { yPercent: -100 }, { duration: 2, yPercent: 100, backgroundColor: '#ffe2c5', repeat: -3, yoyo: true })
